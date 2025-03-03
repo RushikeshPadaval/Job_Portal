fe999f687b986b95e5934976765fb86e6a59bdd2
rushi padaval <rushikeshpadaval@outlook.com>
2025-03-03 19:45:27 +0530

    clerk auth

--- Job-Portal/client/src/main.jsx
+++ Job-Portal/client/src/main.jsx
@@ -14,7 +14,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider   >
+  <ClerkProvider  publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" >
   <BrowserRouter>
   <AppContextProvider>
 
